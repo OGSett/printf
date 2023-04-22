58c3b68d7eabefefc085113c9e4739fa612e82a1
HAJAR <elabdellaoui.hajar@gmail.com>
2023-04-22 23:20:24 +0000

    create tests directory

---
+++ tests/0-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - test %s %c %
+ * Return: 0 always
+ */
+int main(void)
+{
+	char c = 'a';
+	char *str = "Hello world !!";
+	int r_val;
+
+	printf("------------- printf output ----------------\n");
+	r_val = printf("%s %c 100%%\n", str, c);
+	printf("r_val = %d\n", r_val);
+
+	printf("------------- _printf output ----------------\n");
+        r_val = _printf("%s %c 100%%\n", str, c);
+        printf("r_val = %d\n", r_val);
+	return (0);
+}
