66da1de2b2584eb18cae702d1f607af965b8e049
HAJAR <elabdellaoui.hajar@gmail.com>
2023-04-22 22:05:52 +0000

    add specifier struct

--- main.h
+++ main.h
@@ -5,6 +5,17 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+/**
+ * struct specifier - struct specifier
+ * @sp: The specifier
+ * @func: The associated function
+ */
+typedef struct specifier
+{
+	char sp;
+	int (*func)(va_list);
+} sp_t;
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 int (*get_print_func(char sp))(va_list args);
