aa01d5a9fc10f618d67ef510bfffc1c94f3e44ba
HAJAR <elabdellaoui.hajar@gmail.com>
2023-04-23 23:02:04 +0000

    add function that converts unsigned int into a giving base

---
+++ convert.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * convert - converts an integer to a specific base
+ * @n: the giving integer
+ * @base: the base to be converted to
+ * Return: the conversion string
+ */
+char *convert(unsigned int n, int base, int upper_o_n)
+{
+	static char *base_digits;
+	static char buffer[50];
+	char *p_convert;
+
+	base_digits = upper_o_n ? "0123456789ABCDEF" :
+		"0123456789abcdef";
+
+	p_convert = &buffer[49];
+	*p_convert = '\0';
+
+	do {
+		*--p_convert = base_digits[n % base];
+		n /= base;
+	} while (n != 0);
+	return (p_convert);
+}
