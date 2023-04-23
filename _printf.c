22d69f02f1611fcab398fb25218eb58f377f780e
root <OGSett>
2023-04-22 17:17:45 -0700

    task modified

--- _printf.c
+++ _printf.c
@@ -13,12 +13,16 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	while (*(format + i))
 	{
-		if (*(format + i) == '%')
+		if (*(format + i) == '%' && format[i + 1] != '\0')
 		{
 			i++;
 			p_func = get_print_func(*(format + i));
 			if (p_func != NULL)
 				r_val += p_func(args);
+		}
+		else if (format[i] == '%' && format[i + 1] == '\0')
+		{
+			return (-1);
 		}
 		else
 		{
