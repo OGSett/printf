0ce3f1e6902f7964a7aefb5c6deb75ca280d4323
root <OGSett>
2023-04-23 17:06:42 -0700

    get_print_function.c

--- get_print_function.c
+++ get_print_function.c
@@ -12,10 +12,15 @@ int (*get_print_func(char sp))(va_list args)
 		{'s', print_s},
 		{'%', print_percent},
 		{'i', print_n},
-		{'d', print_n}
+		{'d', print_n},
+		{'b', print_b},
+		{'u', print_uint},
+		{'o', print_octal},
+		{'x', print_hex_lower},
+		{'X', print_hex_upper}
 	};
 
-	int specifiers = 5, i = 0;
+	int specifiers = 10, i = 0;
 
 	while (i < specifiers)
 	{
