554c41c014f3aa0a10870b5bb747c019de485a49
root <OGSett>
2023-04-24 19:14:26 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,55 +1,4 @@
 #include "main.h"
-
-/**
- * process_format - Processes the format string
- * and calls appropriate print functions
- * @format: A string that is composed of zero or more directives
- * @args: va_list of arguments
- * @i: Pointer to the index of the current character in the format string
- * @r_val: Pointer to the total number of characters printed
- * Return: The number of characters printed
- */
-void process_format(const char *format, va_list args, int *i, int *r_val)
-{
-	int (*p_func)(va_list, fl_t *);
-	fl_t flags;
-
-	while (*(format + *i))
-	{
-		if (format[*i] == '%' && format[*i + 1] == '\0')
-			return;
-		else if (*(format + *i) == '%')
-		{
-			init_flags(&flags);
-			(*i)++;
-			while (get_flags(*(format + *i), &flags) == 1)
-				(*i)++;
-			flags.zero_pad = 0;
-			if (*(format + *i) == '0')
-			{
-				flags.zero_pad = 1;
-				(*i)++;
-			}
-			p_func = get_print_func(*(format + *i));
-			if (p_func != NULL)
-				*r_val += p_func(args, &flags);
-			else
-			{
-				if ((flags.space == 1 || flags.hash == 1 || flags.plus == 1) &&
-				    *(format + *i) != '\0')
-					*r_val += print_unkown_specifier(&flags, *(format + *i));
-					/*r_val += _printf("%%%c",*(format + i));*/
-			}
-		}
-		else
-		{
-			_putchar(*(format + *i));
-			*r_val += 1;
-		}
-		(*i)++;
-	}
-}
-
 /**
  * _printf - produces output according to a format
  * @format: A string that is composed of zero or more directives
@@ -58,14 +7,48 @@ void process_format(const char *format, va_list args, int *i, int *r_val)
 int _printf(const char *format, ...)
 {
 	va_list args;
+	int (*p_func)(va_list, fl_t *);
 	int i = 0, r_val = 0;
+	fl_t flags;
 
 	if (format == NULL)
 		return (-1);
 	va_start(args, format);
-	process_format(format, args, &i, &r_val);
+	while (*(format + i))
+	{
+		if (format[i] == '%' && format[i + 1] == '\0')
+			return (-1);
+		else if (*(format + i) == '%')
+		{
+			init_flags(&flags);
+			i++;
+			while (get_flags(*(format + i), &flags) == 1)
+				i++;
+			flags.zero_pad = 0;
+			if (*(format + i) == '0')
+			{
+				flags.zero_pad = 1;
+				i++;
+			}
+			p_func = get_print_func(*(format + i));
+			if (p_func != NULL)
+				r_val += p_func(args, &flags);
+			else
+			{
+				if ((flags.space == 1 || flags.hash == 1 || flags.plus == 1) &&
+						*(format + i) != '\0')
+					r_val += print_unkown_specifier(&flags, *(format + i));
+					/*r_val += _printf("%%%c",*(format + i));*/
+			}
+		}
+		else
+		{
+			_putchar(*(format + i));
+			r_val += 1;
+		}
+		i++;
+	}
 	va_end(args);
 	_putchar(-1);
 	return (r_val);
 }
-
